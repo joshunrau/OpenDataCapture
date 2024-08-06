43f8bbf6ee90933072d641da5052c2d6d944d96f
joshunrau <contact@joshuaunrau.com>
2024-08-06 13:03:38 -0400

    refactor: expand

--- runtime/v1/src/opendatacapture@1.0.0/schemas/core.ts
+++ runtime/v1/src/opendatacapture@1.0.0/schemas/core.ts
@@ -1,4 +1,8 @@
 import { z } from '../../zod@3.23.6';
+import { licenses } from '../licenses';
+import { isZodType } from '../utils';
+
+import type { LicenseIdentifier } from '../licenses';
 
 type Language = z.infer<typeof $Language>;
 const $Language = z.enum(['en', 'fr']);
@@ -9,5 +13,13 @@ const $JsonLiteral = z.union([z.string(), z.number(), z.boolean(), z.null()]);
 type Json = { [key: string]: Json } | Json[] | JsonLiteral;
 const $Json: z.ZodType<Json> = z.lazy(() => z.union([$JsonLiteral, z.array($Json), z.record($Json)]));
 
-export { $Json, $Language };
+const $LicenseIdentifier = z.string().refine((arg) => licenses.has(arg as LicenseIdentifier)) as z.ZodType<
+  LicenseIdentifier,
+  z.ZodTypeDef,
+  LicenseIdentifier
+>;
+
+const $ZodTypeAny = z.custom<z.ZodTypeAny>((arg) => isZodType(arg));
+
+export { $Json, $Language, $LicenseIdentifier, $ZodTypeAny };
 export type { Json, Language };
--- runtime/v1/src/opendatacapture@1.0.0/utils.ts
+++ runtime/v1/src/opendatacapture@1.0.0/utils.ts
@@ -1,4 +1,5 @@
 import { uniq } from '../lodash-es@4.17.21';
+import { z } from '../zod@3.23.6';
 
 export const isUnique = (arr: any[]) => uniq(arr).length === arr.length;
 
@@ -6,3 +7,12 @@ export const isObject = (value: unknown): value is object => {
   const type = typeof value;
   return value !== null && (type === 'object' || type === 'function');
 };
+
+/** Used to determine if object is of type `ZodType` independent of specific instances or library versions */
+export function isZodType(arg: unknown): arg is z.ZodTypeAny {
+  let prototype: null | object = null;
+  if (isObject(arg) && isObject(arg.constructor)) {
+    prototype = Reflect.getPrototypeOf(arg.constructor);
+  }
+  return Boolean(prototype && Reflect.get(prototype, 'name') === 'ZodType');
+}
