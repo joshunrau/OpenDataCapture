a30f6f6820030e46be78d60aea413e5cc1d1986f
joshunrau <contact@joshuaunrau.com>
2024-10-17 12:28:30 -0400

    chore: update

---
+++ apps/web/src/features/upload/pages/UploadSelectPage.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Heading } from '@douglasneuroinformatics/libui/components';
+import { useTranslation } from '@douglasneuroinformatics/libui/hooks';
+import { useNavigate } from 'react-router-dom';
+
+import { PageHeader } from '@/components/PageHeader';
+import { useInstrumentInfoQuery } from '@/hooks/useInstrumentInfoQuery';
+
+import { UploadSelectTable } from '../components/UploadSelectTable';
+
+export const UploadSelectPage = () => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const { data } = useInstrumentInfoQuery({
+    params: {
+      kind: 'FORM'
+    }
+  });
+
+  return (
+    <React.Fragment>
+      <PageHeader>
+        <Heading className="text-center" variant="h2">
+          {t({
+            en: 'Select Instrument',
+            fr: 'Selectionnez un instrument'
+          })}
+        </Heading>
+      </PageHeader>
+      <div>
+        <UploadSelectTable
+          data={data ?? []}
+          onSelect={(instrument) => {
+            navigate(instrument.id);
+          }}
+        />
+      </div>
+    </React.Fragment>
+  );
+};
