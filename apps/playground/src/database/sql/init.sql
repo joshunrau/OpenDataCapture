6b58ae983234235abe8fb71f94424a6c15cec7d4
joshunrau <contact@joshuaunrau.com>
2025-01-17 13:22:16 -0500

    chore: add defaults

--- apps/playground/src/database/sql/init.sql
+++ apps/playground/src/database/sql/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS settings (
   id SERIAL PRIMARY KEY,
   api_base_url TEXT,
-  enable_vim_mode BOOLEAN,
-  refresh_interval INTEGER NOT NULL CHECK (refresh_interval > 0)
+  enable_vim_mode BOOLEAN DEFAULT FALSE,
+  refresh_interval INTEGER NOT NULL DEFAULT 2000 CHECK (refresh_interval > 0)
 );
