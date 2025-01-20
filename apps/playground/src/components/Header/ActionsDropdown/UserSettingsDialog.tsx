f1dfe80fe372add3b5af2c4e26614f1b7829f793
joshunrau <contact@joshuaunrau.com>
2025-01-20 14:49:46 -0500

    refactor: move Settings

--- apps/playground/src/components/Header/ActionsDropdown/UserSettingsDialog.tsx
+++ apps/playground/src/components/Header/ActionsDropdown/UserSettingsDialog.tsx
@@ -2,8 +2,9 @@
 
 import { Dialog, Form } from '@douglasneuroinformatics/libui/components';
 import { useNotificationsStore } from '@douglasneuroinformatics/libui/hooks';
+import { z } from 'zod';
 
-import { $Settings } from '@/models/settings.model';
+import { useSettings } from '@/database/hooks/useSettings';
 import { useAppStore } from '@/store';
 
 export type UserSettingsDialogProps = {
@@ -13,8 +14,10 @@ export type UserSettingsDialogProps = {
 
 export const UserSettingsDialog = ({ isOpen, setIsOpen }: UserSettingsDialogProps) => {
   const addNotification = useNotificationsStore((store) => store.addNotification);
-  const settings = useAppStore((store) => store.settings);
+  //const settings = useAppStore((store) => store.settings);
   const updateSettings = useAppStore((store) => store.updateSettings);
+
+  const settings = useSettings();
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
@@ -22,47 +25,57 @@ export const UserSettingsDialog = ({ isOpen, setIsOpen }: UserSettingsDialogProp
         <Dialog.Header>
           <Dialog.Title>User Settings</Dialog.Title>
         </Dialog.Header>
-        <Form
-          className="py-4"
-          content={[
-            {
-              title: 'Default Instance Settings',
-              fields: {
-                apiBaseUrl: {
-                  description: 'The base path for your Open Data Capture REST API.',
-                  kind: 'string',
-                  placeholder: 'e.g., https://demo.opendatacapture.org/api',
-                  label: 'API Base URL',
-                  variant: 'input'
+        {settings && (
+          <Form
+            className="py-4"
+            content={[
+              {
+                title: 'Default Instance Settings',
+                fields: {
+                  apiBaseUrl: {
+                    description: 'The base path for your Open Data Capture REST API.',
+                    kind: 'string',
+                    placeholder: 'e.g., https://demo.opendatacapture.org/api',
+                    label: 'API Base URL',
+                    variant: 'input'
+                  }
+                }
+              },
+              {
+                title: 'Editor Settings',
+                fields: {
+                  enableVimMode: {
+                    kind: 'boolean',
+                    label: 'Enable VIM Mode (Experimental)',
+                    variant: 'radio'
+                  },
+                  refreshInterval: {
+                    description: 'The interval, in milliseconds, between builds, assuming the source code has changed',
+                    kind: 'number',
+                    label: 'Refresh Interval',
+                    variant: 'input'
+                  }
                 }
               }
-            },
-            {
-              title: 'Editor Settings',
-              fields: {
-                enableVimMode: {
-                  kind: 'boolean',
-                  label: 'Enable VIM Mode (Experimental)',
-                  variant: 'radio'
-                },
-                refreshInterval: {
-                  description: 'The interval, in milliseconds, between builds, assuming the source code has changed',
-                  kind: 'number',
-                  label: 'Refresh Interval',
-                  variant: 'input'
-                }
-              }
-            }
-          ]}
-          initialValues={settings}
-          submitBtnLabel="Save Changes"
-          validationSchema={$Settings}
-          onSubmit={(updatedSettings) => {
-            updateSettings(updatedSettings);
-            addNotification({ type: 'success' });
-            setIsOpen(false);
-          }}
-        />
+            ]}
+            initialValues={settings}
+            submitBtnLabel="Save Changes"
+            validationSchema={z.object({
+              apiBaseUrl: z
+                .string()
+                .optional()
+                .transform((arg) => (arg === '' ? undefined : arg))
+                .pipe(z.string().url().optional()),
+              enableVimMode: z.boolean().optional(),
+              refreshInterval: z.number().positive().int()
+            })}
+            onSubmit={(updatedSettings) => {
+              updateSettings(updatedSettings);
+              addNotification({ type: 'success' });
+              setIsOpen(false);
+            }}
+          />
+        )}
       </Dialog.Content>
     </Dialog>
   );
--- apps/playground/src/models/settings.model.ts
+++ apps/playground/src/models/settings.model.ts
@@ -1,9 +1,5 @@
-import { z } from 'zod';
-
-export const $Settings = z.object({
-  apiBaseUrl: z.string().nullish(),
-  enableVimMode: z.boolean().nullish(),
-  refreshInterval: z.number().positive().int()
-});
-
-export type Settings = z.infer<typeof $Settings>;
+export type Settings = {
+  apiBaseUrl?: null | string | undefined;
+  enableVimMode?: boolean | null | undefined;
+  refreshInterval: number;
+};
