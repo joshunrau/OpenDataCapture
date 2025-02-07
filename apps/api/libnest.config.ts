a44de544e98951360a3e6ac030726f205bd52c94
joshunrau <contact@joshuaunrau.com>
2025-02-07 16:45:53 -0500

    chore: inject globals

--- apps/api/libnest.config.ts
+++ apps/api/libnest.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from '@douglasneuroinformatics/libnest';
+import { getReleaseInfo } from '@opendatacapture/release-info';
 
 export default defineConfig({
-  entry: 'src/main.ts'
+  entry: 'src/main.ts',
+  globals: {
+    __RELEASE__: await getReleaseInfo()
+  }
 });
