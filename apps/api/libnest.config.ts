23585d72899cc15386c28e36beaf40b060198143
joshunrau <contact@joshuaunrau.com>
2025-02-12 11:15:03 -0500

    chore: update

--- apps/api/libnest.config.ts
+++ apps/api/libnest.config.ts
@@ -1,5 +1,14 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+
 import { defineUserConfig } from '@douglasneuroinformatics/libnest/user-config';
 import { getReleaseInfo } from '@opendatacapture/release-info';
+
+import type { Config } from '@/core/config';
+
+declare module '@douglasneuroinformatics/libnest/user-config' {
+  export interface RuntimeConfig extends Config {}
+}
 
 export default defineUserConfig({
   entry: 'src/main.ts',
