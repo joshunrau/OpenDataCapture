9eeaeaf8c06b35c44f41a668d65df7a7f4a898fd
joshunrau <contact@joshuaunrau.com>
2025-02-14 10:25:18 -0500

    chore: update

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,5 @@
+import * as path from 'node:path';
+
 import { AppFactory } from '@douglasneuroinformatics/libnest/core';
 
 import { $Config } from './config';
@@ -5,11 +7,34 @@ import { $Config } from './config';
 export default async function main() {
   await AppFactory.createApp({
     callback: async (app, config, logger) => {
+      app.useStaticAssets(path.resolve(import.meta.dirname, '..', 'public'));
       const isProduction = config.NODE_ENV === 'production';
       const port = config[isProduction ? 'API_PROD_SERVER_PORT' : 'API_DEV_SERVER_PORT'];
       await app.listen(port);
       const url = await app.getUrl();
       logger.log(`Application is running on: ${url}`);
+    },
+    docs: {
+      config: {
+        contact: {
+          email: 'support@douglasneuroinformatics.ca',
+          name: 'Douglas Neuroinformatics',
+          url: 'https://douglasneuroinformatics.ca'
+        },
+        description: 'Documentation for the REST API for Open Data Capture',
+        externalDoc: {
+          description: 'Homepage',
+          url: 'https://opendatacapture.org'
+        },
+        license: {
+          name: 'Apache-2.0',
+          url: 'https://www.apache.org/licenses/LICENSE-2.0'
+        },
+        tags: ['Authentication', 'Groups', 'Instruments', 'Instrument Records', 'Subjects', 'Users'],
+        title: 'Open Data Capture',
+        version: '1'
+      },
+      path: '/spec.json'
     },
     modules: [],
     schema: $Config,
