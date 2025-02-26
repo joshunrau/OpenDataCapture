f3d65149b2c75929215c2de19927ad672c252dfc
joshunrau <contact@joshuaunrau.com>
2025-02-26 12:38:33 -0500

    chore: update

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -1,13 +1,37 @@
-import { $BooleanLike, $NumberLike } from '@douglasneuroinformatics/libjs';
-import { $BaseEnv } from 'node_modules/@douglasneuroinformatics/libnest/dist/config/schema';
+import { $BooleanLike, $NumberLike, $UrlLike } from '@douglasneuroinformatics/libjs';
+import { $BaseEnv } from '@douglasneuroinformatics/libnest/config';
 import { z } from 'zod';
 
 export type Config = z.infer<typeof $Config>;
-export const $Config = $BaseEnv.extend({
-  GATEWAY_API_KEY: z.string().min(32),
-  GATEWAY_DEV_SERVER_PORT: $NumberLike.pipe(z.number().positive().int()).optional(),
-  GATEWAY_ENABLED: $BooleanLike
-  // GATEWAY_INTERNAL_NETWORK_URL: $ParsedURL.optional(),
-  // GATEWAY_REFRESH_INTERVAL: $ParsedNumber(z.number().positive().int()),
-  // GATEWAY_SITE_ADDRESS: $ParsedURL.optional()
-});
+export const $Config = $BaseEnv
+  .extend({
+    GATEWAY_API_KEY: z.string().min(32),
+    GATEWAY_DEV_SERVER_PORT: $NumberLike.pipe(z.number().positive().int()).optional(),
+    GATEWAY_ENABLED: $BooleanLike,
+    GATEWAY_INTERNAL_NETWORK_URL: $UrlLike.optional(),
+    GATEWAY_REFRESH_INTERVAL: $NumberLike.pipe(z.number().positive().int()),
+    GATEWAY_SITE_ADDRESS: $UrlLike.optional()
+  })
+  .superRefine((env, ctx) => {
+    if (env.NODE_ENV === 'production') {
+      if (!env.GATEWAY_SITE_ADDRESS) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'GATEWAY_SITE_ADDRESS must be defined in production'
+        });
+      }
+    } else if (env.NODE_ENV === 'development') {
+      if (!env.API_DEV_SERVER_PORT) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'API_DEV_SERVER_PORT must be defined in development'
+        });
+      }
+      if (!env.GATEWAY_DEV_SERVER_PORT) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'GATEWAY_DEV_SERVER_PORT must be defined in development'
+        });
+      }
+    }
+  });
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,53 +1,78 @@
-import * as path from 'node:path';
-
 import { AppFactory } from '@douglasneuroinformatics/libnest/core';
 // import { APP_GUARD } from '@nestjs/core';
 
 import { $Config } from './config';
 
-export default async function main() {
-  await AppFactory.createApp({
-    callback: async (app, config, logger) => {
-      app.useStaticAssets(path.resolve(import.meta.dirname, '..', 'public'));
-      const isProduction = config.NODE_ENV === 'production';
-      const port = config[isProduction ? 'API_PROD_SERVER_PORT' : 'API_DEV_SERVER_PORT'];
-      await app.listen(port);
-      const url = await app.getUrl();
-      logger.log(`Application is running on: ${url}`);
-    },
-    docs: {
-      config: {
-        contact: {
-          email: 'support@douglasneuroinformatics.ca',
-          name: 'Douglas Neuroinformatics',
-          url: 'https://douglasneuroinformatics.ca'
-        },
-        description: 'Documentation for the REST API for Open Data Capture',
-        externalDoc: {
-          description: 'Homepage',
-          url: 'https://opendatacapture.org'
-        },
-        license: {
-          name: 'Apache-2.0',
-          url: 'https://www.apache.org/licenses/LICENSE-2.0'
-        },
-        tags: ['Authentication', 'Groups', 'Instruments', 'Instrument Records', 'Subjects', 'Users'],
-        title: 'Open Data Capture',
-        version: '1'
+export default AppFactory.create({
+  docs: {
+    config: {
+      contact: {
+        email: 'support@douglasneuroinformatics.ca',
+        name: 'Douglas Neuroinformatics',
+        url: 'https://douglasneuroinformatics.ca'
       },
-      path: '/spec.json'
+      description: 'Documentation for the REST API for Open Data Capture',
+      externalDoc: {
+        description: 'Homepage',
+        url: 'https://opendatacapture.org'
+      },
+      license: {
+        name: 'Apache-2.0',
+        url: 'https://www.apache.org/licenses/LICENSE-2.0'
+      },
+      tags: ['Authentication', 'Groups', 'Instruments', 'Instrument Records', 'Subjects', 'Users'],
+      title: 'Open Data Capture',
+      version: '1'
     },
-    // providers: [
-    //   {
-    //     provide: APP_GUARD,
-    //     useClass: AuthenticationGuard
-    //   },
-    //   {
-    //     provide: APP_GUARD,
-    //     useClass: AuthorizationGuard
-    //   }
-    // ],
-    schema: $Config,
-    version: '1'
-  });
-}
+    path: '/spec.json'
+  },
+  schema: $Config,
+  version: '1'
+});
+
+// export default async function main() {
+//   await AppFactory.createApp({
+//     callback: async (app, config, logger) => {
+//       app.useStaticAssets(path.resolve(import.meta.dirname, '..', 'public'));
+//       const isProduction = config.NODE_ENV === 'production';
+//       const port = config[isProduction ? 'API_PROD_SERVER_PORT' : 'API_DEV_SERVER_PORT'];
+//       await app.listen(port);
+//       const url = await app.getUrl();
+//       logger.log(`Application is running on: ${url}`);
+//     },
+//     docs: {
+//       config: {
+//         contact: {
+//           email: 'support@douglasneuroinformatics.ca',
+//           name: 'Douglas Neuroinformatics',
+//           url: 'https://douglasneuroinformatics.ca'
+//         },
+//         description: 'Documentation for the REST API for Open Data Capture',
+//         externalDoc: {
+//           description: 'Homepage',
+//           url: 'https://opendatacapture.org'
+//         },
+//         license: {
+//           name: 'Apache-2.0',
+//           url: 'https://www.apache.org/licenses/LICENSE-2.0'
+//         },
+//         tags: ['Authentication', 'Groups', 'Instruments', 'Instrument Records', 'Subjects', 'Users'],
+//         title: 'Open Data Capture',
+//         version: '1'
+//       },
+//       path: '/spec.json'
+//     },
+//     // providers: [
+//     //   {
+//     //     provide: APP_GUARD,
+//     //     useClass: AuthenticationGuard
+//     //   },
+//     //   {
+//     //     provide: APP_GUARD,
+//     //     useClass: AuthorizationGuard
+//     //   }
+//     // ],
+//     schema: $Config,
+//     version: '1'
+//   });
+// }
