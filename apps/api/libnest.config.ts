73136186df197a3b50df19a7d3633c7d6df04927
joshunrau <contact@joshuaunrau.com>
2025-02-26 12:34:22 -0500

    chore: update

--- apps/api/libnest.config.ts
+++ apps/api/libnest.config.ts
@@ -1,16 +1,17 @@
-/* eslint-disable @typescript-eslint/no-empty-object-type */
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 
-import { defineUserConfig } from '@douglasneuroinformatics/libnest/user-config';
+import { defineConfig } from '@douglasneuroinformatics/libnest/config';
 import { getReleaseInfo } from '@opendatacapture/release-info';
 
 import type { Config } from '@/config';
 
-declare module '@douglasneuroinformatics/libnest/user-config' {
-  export interface RuntimeConfig extends Config {}
+declare module '@douglasneuroinformatics/libnest/config' {
+  export interface RuntimeEnv extends Config {
+    foo: string;
+  }
 }
 
-export default defineUserConfig({
+export default defineConfig({
   entry: 'src/main.ts',
   globals: {
     __RELEASE__: await getReleaseInfo()
--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -1,8 +1,13 @@
-import { $BooleanLike } from '@douglasneuroinformatics/libjs';
-import { $BaseRuntimeConfig } from '@douglasneuroinformatics/libnest/core';
-import type { z } from 'zod';
+import { $BooleanLike, $NumberLike } from '@douglasneuroinformatics/libjs';
+import { $BaseEnv } from 'node_modules/@douglasneuroinformatics/libnest/dist/config/schema';
+import { z } from 'zod';
 
 export type Config = z.infer<typeof $Config>;
-export const $Config = $BaseRuntimeConfig.extend({
-  DANGEROUSLY_DISABLE_PBKDF2_ITERATION: $BooleanLike
+export const $Config = $BaseEnv.extend({
+  GATEWAY_API_KEY: z.string().min(32),
+  GATEWAY_DEV_SERVER_PORT: $NumberLike.pipe(z.number().positive().int()).optional(),
+  GATEWAY_ENABLED: $BooleanLike
+  // GATEWAY_INTERNAL_NETWORK_URL: $ParsedURL.optional(),
+  // GATEWAY_REFRESH_INTERVAL: $ParsedNumber(z.number().positive().int()),
+  // GATEWAY_SITE_ADDRESS: $ParsedURL.optional()
 });
