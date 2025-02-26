cf00841bc278f325dfedc153e5ade5b5242eb7a3
joshunrau <contact@joshuaunrau.com>
2025-02-26 12:39:41 -0500

    chore: update

---
+++ apps/api/src/core/types.ts
@@ -0,0 +1,18 @@
+import type { PureAbility } from '@casl/ability';
+import type { PrismaQuery } from '@casl/prisma';
+import type { AppAction, AppSubjectName } from '@opendatacapture/schemas/core';
+import type {
+  GroupModel,
+  InstrumentRecordModel,
+  SessionModel,
+  SubjectModel,
+  UserModel
+} from '@prisma/generated-client';
+
+type AppSubject = AppSubjectName | GroupModel | InstrumentRecordModel | SessionModel | SubjectModel | UserModel;
+
+export type AppAbility = PureAbility<[AppAction, AppSubject], PrismaQuery>;
+
+export type EntityOperationOptions = {
+  ability?: AppAbility;
+};
