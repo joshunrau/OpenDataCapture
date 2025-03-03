d571e67a2663cc01fb9c76fa78e02de55c12e4dd
joshunrau <contact@joshuaunrau.com>
2025-03-03 17:27:51 -0500

    chore: update

---
+++ apps/api/src/groups/__tests__/groups.service.spec.ts
@@ -0,0 +1,62 @@
+import type { Model } from '@douglasneuroinformatics/libnest/core';
+import { getModelToken } from '@douglasneuroinformatics/libnest/core';
+import { type MockedInstance, MockFactory } from '@douglasneuroinformatics/libnest/testing';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import { InstrumentsService } from '@/instruments/instruments.service';
+
+import { GroupsService } from '../groups.service';
+
+describe('GroupsService', () => {
+  let groupsService: GroupsService;
+  let groupModel: MockedInstance<Model<'GroupModel'>>;
+  let instrumentsService: MockedInstance<InstrumentsService>;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        GroupsService,
+        MockFactory.createForModelToken(getModelToken('GroupModel')),
+        MockFactory.createForService(InstrumentsService)
+      ]
+    }).compile();
+    groupModel = moduleRef.get(getModelToken('GroupModel'));
+    groupsService = moduleRef.get(GroupsService);
+    instrumentsService = moduleRef.get(InstrumentsService);
+    instrumentsService.find.mockResolvedValue([]);
+  });
+
+  describe('create', () => {
+    it('should call the group model', async () => {
+      await groupsService.create({ name: 'Test Group', type: 'CLINICAL' });
+      expect(groupModel.create.mock.lastCall?.[0]).toMatchObject({ data: { name: 'Test Group' } });
+    });
+
+    it('should throw a ConflictException if a group with the same name already exists in the db', async () => {
+      groupModel.exists.mockResolvedValueOnce(true);
+      await expect(groupsService.create({ name: 'Test Group', type: 'CLINICAL' })).rejects.toBeInstanceOf(
+        ConflictException
+      );
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return the array returned by the group model', async () => {
+      groupModel.findMany.mockResolvedValueOnce([{ name: 'Test Group' }]);
+      await expect(groupsService.findAll()).resolves.toMatchObject([{ name: 'Test Group' }]);
+    });
+  });
+
+  describe('findById', () => {
+    it('should throw a `NotFoundException` if there is no group with the provided id', async () => {
+      groupModel.findFirst.mockResolvedValueOnce(null);
+      await expect(groupsService.findById('123')).rejects.toBeInstanceOf(NotFoundException);
+    });
+    it('should return the group with the provided id if it exists', async () => {
+      groupModel.findFirst.mockResolvedValueOnce({ name: 'Test Group' });
+      await expect(groupsService.findById('123')).resolves.toMatchObject({ name: 'Test Group' });
+    });
+  });
+});
