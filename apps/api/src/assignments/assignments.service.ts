28da329bbe33fa7e4f06a4c8c6e7ad3a6999957b
joshunrau <contact@joshuaunrau.com>
2025-03-03 18:18:45 -0500

    chore: update

---
+++ apps/api/src/assignments/assignments.service.ts
@@ -0,0 +1,103 @@
+import crypto from 'node:crypto';
+
+import { HybridCrypto } from '@douglasneuroinformatics/libcrypto';
+import { ConfigService, InjectModel, type Model } from '@douglasneuroinformatics/libnest/core';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import type { Assignment, UpdateAssignmentData } from '@opendatacapture/schemas/assignment';
+
+import { accessibleQuery } from '@/ability/ability.utils';
+import type { EntityOperationOptions } from '@/core/types';
+import { GatewayService } from '@/gateway/gateway.service';
+
+import { CreateAssignmentDto } from './dto/create-assignment.dto';
+
+@Injectable()
+export class AssignmentsService {
+  private readonly assignmentBaseUrl: string;
+
+  constructor(
+    @InjectModel('AssignmentModel') private readonly assignmentModel: Model<'AssignmentModel'>,
+    configService: ConfigService,
+    private readonly gatewayService: GatewayService
+  ) {
+    if (configService.get('NODE_ENV') === 'production') {
+      const siteAddress = configService.getOrThrow('GATEWAY_SITE_ADDRESS');
+      this.assignmentBaseUrl = siteAddress.origin;
+    } else {
+      const gatewayPort = configService.get('GATEWAY_DEV_SERVER_PORT');
+      this.assignmentBaseUrl = `http://localhost:${gatewayPort}`;
+    }
+  }
+
+  async create({ expiresAt, groupId, instrumentId, subjectId }: CreateAssignmentDto): Promise<Assignment> {
+    const { privateKey, publicKey } = await HybridCrypto.generateKeyPair();
+    const id = crypto.randomUUID();
+    const assignment = await this.assignmentModel.create({
+      data: {
+        encryptionKeyPair: {
+          privateKey: Buffer.from(await HybridCrypto.serializePrivateKey(privateKey)),
+          publicKey: Buffer.from(await HybridCrypto.serializePublicKey(publicKey))
+        },
+        expiresAt,
+        group: groupId
+          ? {
+              connect: {
+                id: groupId
+              }
+            }
+          : undefined,
+        id,
+        instrument: {
+          connect: {
+            id: instrumentId
+          }
+        },
+        status: 'OUTSTANDING',
+        subject: {
+          connect: {
+            id: subjectId
+          }
+        },
+        url: `${this.assignmentBaseUrl}/assignments/${id}`
+      }
+    });
+    try {
+      await this.gatewayService.createRemoteAssignment(assignment, publicKey);
+    } catch (err) {
+      await this.assignmentModel.delete({ where: { id } });
+      throw err;
+    }
+    return assignment;
+  }
+
+  async find(
+    { subjectId }: { subjectId?: string } = {},
+    { ability }: EntityOperationOptions = {}
+  ): Promise<Assignment[]> {
+    return this.assignmentModel.findMany({
+      where: {
+        AND: [accessibleQuery(ability, 'read', 'AssignmentModel'), { subjectId }]
+      }
+    });
+  }
+
+  async findById(id: string, { ability }: EntityOperationOptions = {}) {
+    const assignment = await this.assignmentModel.findFirst({
+      where: { AND: [accessibleQuery(ability, 'read', 'AssignmentModel')], id }
+    });
+    if (!assignment) {
+      throw new NotFoundException(`Failed to find assignment with ID: ${id}`);
+    }
+    return assignment;
+  }
+
+  async updateById(id: string, data: UpdateAssignmentData, { ability }: EntityOperationOptions = {}) {
+    if (data.status === 'CANCELED') {
+      await this.gatewayService.deleteRemoteAssignment(id);
+    }
+    return this.assignmentModel.update({
+      data,
+      where: { AND: [accessibleQuery(ability, 'update', 'AssignmentModel')], id }
+    });
+  }
+}
