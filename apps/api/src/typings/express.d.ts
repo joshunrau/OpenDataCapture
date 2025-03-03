c6ad87a1de5ddb017223d367bd9db7da29e52daf
joshunrau <contact@joshuaunrau.com>
2025-03-03 17:16:51 -0500

    chore: update

---
+++ apps/api/src/typings/express.d.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+
+import type { UserModel } from '@prisma/generated-client';
+
+import type { AppAbility } from '@/core/types';
+
+declare global {
+  namespace Express {
+    // https://stackoverflow.com/questions/66312048/cant-override-express-request-user-type-but-i-can-add-new-properties-to-request
+    interface User extends UserModel {
+      ability: AppAbility;
+    }
+  }
+}
---
+++ apps/api/src/typings/prisma-json-types-generator.d.ts
@@ -0,0 +1,7 @@
+import type { InstrumentMeasureValue } from '@opendatacapture/runtime-core';
+
+declare global {
+  namespace PrismaJson {
+    type ComputedMeasures = { [key: string]: InstrumentMeasureValue } | null | undefined;
+  }
+}
