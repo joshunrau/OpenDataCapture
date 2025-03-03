249db857f1edbc97efde2f4c75bfc2d286480013
joshunrau <contact@joshuaunrau.com>
2025-03-03 15:08:46 -0500

    chore: update

--- apps/api/src/instruments/instruments.module.ts
+++ apps/api/src/instruments/instruments.module.ts
@@ -2,12 +2,18 @@ import { VirtualizationModule } from '@douglasneuroinformatics/libnest/virtualiz
 import { Module } from '@nestjs/common';
 
 import { InstrumentsController } from './instruments.controller';
-import { InstrumentsService } from './instruments.service';
+import { InstrumentsService, type InstrumentVirtualizationContext } from './instruments.service';
 
 @Module({
   controllers: [InstrumentsController],
   exports: [InstrumentsService],
-  imports: [VirtualizationModule],
+  imports: [
+    VirtualizationModule.forRoot({
+      context: {
+        instruments: new Map()
+      } satisfies InstrumentVirtualizationContext
+    })
+  ],
   providers: [InstrumentsService]
 })
 export class InstrumentsModule {}
