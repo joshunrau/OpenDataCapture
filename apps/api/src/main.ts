a3049c285d6b2183258cab1e36ecf24977f46fc5
joshunrau <contact@joshuaunrau.com>
2025-03-07 16:30:11 -0500

    chore: update

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -18,14 +18,9 @@ import { UsersModule } from './users/users.module';
 
 export default AppContainer.create({
   auth: {
-    userQueryFactory: () => {
-      return (username) => {
-        if (username !== 'admin') {
-          return null;
-        }
-        return { hashedPassword: '', tokenPayload: {} };
-      };
-    }
+    enabled: true,
+    tokenPayload: ['basePermissionLevel'],
+    userModel: 'UserModel'
   },
   docs: {
     config: {
@@ -83,50 +78,3 @@ export default AppContainer.create({
   ],
   version: '1'
 });
-
-// export default async function main() {
-//   await AppFactory.createApp({
-//     callback: async (app, config, logger) => {
-//       app.useStaticAssets(path.resolve(import.meta.dirname, '..', 'public'));
-//       const isProduction = config.NODE_ENV === 'production';
-//       const port = config[isProduction ? 'API_PROD_SERVER_PORT' : 'API_DEV_SERVER_PORT'];
-//       await app.listen(port);
-//       const url = await app.getUrl();
-//       logger.log(`Application is running on: ${url}`);
-//     },
-//     docs: {
-//       config: {
-//         contact: {
-//           email: 'support@douglasneuroinformatics.ca',
-//           name: 'Douglas Neuroinformatics',
-//           url: 'https://douglasneuroinformatics.ca'
-//         },
-//         description: 'Documentation for the REST API for Open Data Capture',
-//         externalDoc: {
-//           description: 'Homepage',
-//           url: 'https://opendatacapture.org'
-//         },
-//         license: {
-//           name: 'Apache-2.0',
-//           url: 'https://www.apache.org/licenses/LICENSE-2.0'
-//         },
-//         tags: ['Authentication', 'Groups', 'Instruments', 'Instrument Records', 'Subjects', 'Users'],
-//         title: 'Open Data Capture',
-//         version: '1'
-//       },
-//       path: '/spec.json'
-//     },
-//     // providers: [
-//     //   {
-//     //     provide: APP_GUARD,
-//     //     useClass: AuthenticationGuard
-//     //   },
-//     //   {
-//     //     provide: APP_GUARD,
-//     //     useClass: AuthorizationGuard
-//     //   }
-//     // ],
-//     schema: $Config,
-//     version: '1'
-//   });
-// }
