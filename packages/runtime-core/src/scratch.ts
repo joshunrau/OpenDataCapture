53024472e1bda45b1630256650ef1c3bdf86295f
joshunrau <contact@joshuaunrau.com>
2025-06-03 13:08:39 -0400

    chore: update

--- packages/runtime-core/src/scratch.ts
+++ packages/runtime-core/src/scratch.ts
@@ -1,12 +1,29 @@
 /* eslint-disable */
 
 import type { InstrumentKind, InstrumentLanguage, InstrumentSchemaVersion } from './types/instrument.base.js';
+import type { FormInstrument } from './types/instrument.form.js';
+import type { InteractiveInstrument } from './types/instrument.interactive.js';
 
 type BaseInstrumentDef = {
   kind: InstrumentKind;
   language: InstrumentLanguage;
   schemaVersion: InstrumentSchemaVersion;
 };
+
+type DiscriminatedInstrument<
+  TKind extends InstrumentKind,
+  TLanguage extends InstrumentLanguage,
+  TSchemaVersion extends InstrumentSchemaVersion,
+  TData
+> = [TKind] extends ['FORM']
+  ? TData extends FormInstrument.Data
+    ? FormInstrument<TData, TLanguage, TSchemaVersion>
+    : never
+  : [TKind] extends ['INTERACTIVE']
+    ? TData extends InteractiveInstrument.Data
+      ? InteractiveInstrument<TData, TLanguage, TSchemaVersion>
+      : never
+    : never;
 
 type InstrumentDef<T> = BaseInstrumentDef &
   T &
