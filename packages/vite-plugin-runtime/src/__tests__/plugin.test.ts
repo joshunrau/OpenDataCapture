39830a9ce5cae9c573e3512210412e8cbc52f937
joshunrau <contact@joshuaunrau.com>
2025-06-27 16:02:36 -0400

    chore: update

--- packages/vite-plugin-runtime/src/__tests__/plugin.test.ts
+++ packages/vite-plugin-runtime/src/__tests__/plugin.test.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 
 import { beforeAll, describe, expect, it, vi } from 'vitest';
 
-import { plugin, RUNTIME_DIR, RUNTIME_DIST_DIRNAME } from '../plugin';
+import { generateMetadataForVersion, plugin, RUNTIME_DIR, RUNTIME_DIST_DIRNAME } from '../plugin';
 
 const fs = vi.hoisted(() => ({
   existsSync: vi.fn().mockReturnValue(true),
@@ -10,67 +10,38 @@ const fs = vi.hoisted(() => ({
     isDirectory: () => !filepath.split('/').at(-1)!.includes('.')
   })),
   promises: {
+    cp: vi.fn(),
     readdir: vi.fn().mockImplementation((filepath: string) => {
       if (filepath === RUNTIME_DIR) {
-        return ['v1'];
+        return ['v1', '.DS_Store'];
       } else if (filepath === path.join(RUNTIME_DIR, 'v1', RUNTIME_DIST_DIRNAME)) {
         return ['@opendatacapture'];
       } else if (filepath.endsWith('@opendatacapture')) {
         return ['runtime-core'];
       } else if (filepath.endsWith('runtime-core')) {
-        return ['index.js', 'index.d.ts'];
+        return ['index.js', 'index.d.ts', 'styles'];
+      } else if (filepath.endsWith('styles')) {
+        return ['index.css'];
       }
       throw new Error(`Unexpected filepath for test mock: ${filepath}`);
-    })
+    }),
+    writeFile: vi.fn()
   }
 }));
 
 vi.mock('fs', () => fs);
 
-// describe('resolveVersion', () => {
-//   beforeEach(() => {
-//     vi.spyOn(fs.promises, 'readdir').mockResolvedValueOnce(['index.js', 'index.d.ts'] as any);
-//     vi.spyOn(fs, 'existsSync').mockReturnValue(true);
-//     vi.spyOn(fs.promises, 'lstat').mockImplementation((filepath) => {
-//       return Promise.resolve({ isDirectory: () => !path.extname(filepath as string) } as any);
-//     });
-//   });
+describe('generateMetadataForVersion', () => {
+  it('should throw if the resolved baseDir does not exist', async () => {
+    fs.existsSync.mockReturnValueOnce(false);
+    await expect(() => generateMetadataForVersion('v0')).rejects.toThrow('Not a directory');
+  });
 
-//   afterEach(() => {
-//     vi.clearAllMocks();
-//   });
-
-//   it('should throw if the resolved baseDir does not exist', async () => {
-//     vi.spyOn(fs, 'existsSync').mockReturnValueOnce(false);
-//     await expect(() => resolveVersion('v0')).rejects.toThrow('Not a directory');
-//   });
-
-//   it('should throw if the resolved baseDir is not a directory', async () => {
-//     vi.spyOn(fs.promises, 'lstat').mockResolvedValueOnce({ isDirectory: () => false } as any);
-//     await expect(() => resolveVersion('v0')).rejects.toThrow('Not a directory');
-//   });
-//   it('should sort the manifest files appropriately', async () => {
-//     await expect(resolveVersion('v0')).resolves.toMatchObject({
-//       manifest: {
-//         declarations: ['index.d.ts'],
-//         sources: ['index.js']
-//       }
-//     });
-//   });
-//   it('should recurse into directories', async () => {
-//     vi.spyOn(fs.promises, 'readdir')
-//       .mockResolvedValueOnce(['index.js', 'index.d.ts', 'utils'] as any)
-//       .mockResolvedValueOnce(['foo.js'] as any);
-//     await expect(resolveVersion('v0')).resolves.toMatchObject({
-//       importPaths: ['/runtime/v0/index.js', '/runtime/v0/utils/foo.js'],
-//       manifest: {
-//         declarations: ['index.d.ts'],
-//         sources: ['index.js', 'utils/foo.js']
-//       },
-//       version: 'v0'
-//     });
-//   });
-// });
+  it('should throw if the resolved baseDir is not a directory', async () => {
+    fs.lstatSync.mockReturnValueOnce({ isDirectory: () => false });
+    await expect(() => generateMetadataForVersion('v0')).rejects.toThrow('Not a directory');
+  });
+});
 
 describe('plugin', () => {
   let result: any;
@@ -81,6 +52,25 @@ describe('plugin', () => {
 
   it('should return false if disabled', async () => {
     await expect(plugin({ disabled: true })).resolves.toBe(false);
+  });
+
+  describe('buildStart', () => {
+    it('should copy the runtime dist and the manifest to the output dir', async () => {
+      await result.buildStart();
+      const destination = path.join(process.cwd(), 'dist/runtime/v1');
+      expect(fs.promises.cp).toHaveBeenCalledOnce();
+      expect(fs.promises.cp).toHaveBeenLastCalledWith(path.join(RUNTIME_DIR, 'v1', RUNTIME_DIST_DIRNAME), destination, {
+        recursive: true
+      });
+      expect(fs.promises.writeFile).toHaveBeenCalledOnce();
+      expect(fs.promises.writeFile).toHaveBeenLastCalledWith(expect.any(String), expect.any(String), 'utf-8');
+      const manifest = JSON.parse(fs.promises.writeFile.mock.lastCall![1] as string);
+      expect(manifest).toStrictEqual({
+        declarations: ['@opendatacapture/runtime-core/index.d.ts'],
+        sources: ['@opendatacapture/runtime-core/index.js'],
+        styles: ['@opendatacapture/runtime-core/styles/index.css']
+      });
+    });
   });
 
   describe('config', () => {
