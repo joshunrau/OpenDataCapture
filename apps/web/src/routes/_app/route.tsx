af472279f17f0b2ade5e78686eac33ef25298a60
joshunrau <contact@joshuaunrau.com>
2025-07-14 13:47:55 -0400

    chore: update

--- apps/web/src/routes/_app/route.tsx
+++ apps/web/src/routes/_app/route.tsx
@@ -1,12 +1,17 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 import { setupStateQueryOptions } from '@/hooks/useSetupStateQuery';
+import { useAppStore } from '@/store';
 
 export const Route = createFileRoute('/_app')({
   beforeLoad: async ({ context }) => {
     const setupState = await context.queryClient.fetchQuery(setupStateQueryOptions());
     if (!setupState.isSetup) {
       throw redirect({ to: '/setup' });
+    }
+    const { accessToken } = useAppStore.getState();
+    if (!accessToken) {
+      throw redirect({ to: '/auth/login' });
     }
   }
 });
