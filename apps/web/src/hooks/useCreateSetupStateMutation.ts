edd1badf6d1594fb7547d525818bb35c4a20c1d8
joshunrau <contact@joshuaunrau.com>
2025-07-14 12:43:41 -0400

    chore: update

--- apps/web/src/hooks/useCreateSetupStateMutation.ts
+++ apps/web/src/hooks/useCreateSetupStateMutation.ts
@@ -3,14 +3,16 @@ import type { InitAppOptions } from '@opendatacapture/schemas/setup';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
-export function useCreateSetupState() {
+import { SETUP_STATE_QUERY_KEY } from './useSetupStateQuery';
+
+export function useCreateSetupStateMutation() {
   const queryClient = useQueryClient();
   const addNotification = useNotificationsStore((store) => store.addNotification);
   return useMutation({
     mutationFn: (data: InitAppOptions) => axios.post('/v1/setup', data),
-    onSuccess() {
+    onSuccess: async () => {
       addNotification({ type: 'success' });
-      void queryClient.invalidateQueries({ queryKey: ['setup-state'] });
+      await queryClient.invalidateQueries({ queryKey: [SETUP_STATE_QUERY_KEY] });
     }
   });
 }
--- apps/web/src/routes/setup.tsx
+++ apps/web/src/routes/setup.tsx
@@ -1,6 +1,14 @@
+import { useTheme } from '@douglasneuroinformatics/libui/hooks';
 import { createFileRoute } from '@tanstack/react-router';
 
+import { useSetupStateQuery } from '@/hooks/useSetupStateQuery';
+
 const RouteComponent = () => {
+  const setupStateQuery = useSetupStateQuery();
+
+  // since there is no theme toggle on the page, this is required to set the document attribute
+  useTheme();
+
   return <div>Setup</div>;
 };
 
