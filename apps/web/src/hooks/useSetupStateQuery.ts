96343f9659627884a209b45da407c8b394ab17cf
joshunrau <contact@joshuaunrau.com>
2025-07-14 12:35:00 -0400

    chore: update

--- apps/web/src/hooks/useSetupStateQuery.ts
+++ apps/web/src/hooks/useSetupStateQuery.ts
@@ -2,13 +2,15 @@ import { $SetupState } from '@opendatacapture/schemas/setup';
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
+export const SETUP_STATE_QUERY_KEY = 'setup-state';
+
 export const setupStateQueryOptions = () => {
   return queryOptions({
     queryFn: async () => {
       const response = await axios.get('/v1/setup');
       return $SetupState.parseAsync(response.data);
     },
-    queryKey: ['setup-state']
+    queryKey: [SETUP_STATE_QUERY_KEY]
   });
 };
 
