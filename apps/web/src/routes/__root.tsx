6bd73370d800ba731b51021d2e88164d3d828ec6
joshunrau <contact@joshuaunrau.com>
2025-07-14 12:15:25 -0400

    chore: update

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,4 +1,8 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { Fragment } from 'react';
+
+import { NotificationHub } from '@douglasneuroinformatics/libui/components';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 import '../services/axios';
@@ -7,18 +11,24 @@ import '../services/zod';
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div className="flex gap-2 p-2">
-        <Link className="[&.active]:font-bold" to="/">
-          Home
-        </Link>{' '}
-        <Link className="[&.active]:font-bold" to="/about">
-          About
-        </Link>
-      </div>
-      <hr />
+    <Fragment>
       <Outlet />
+      <NotificationHub />
+      <ReactQueryDevtools buttonPosition="bottom-right" position="left" />
       <TanStackRouterDevtools />
-    </>
+    </Fragment>
+    // <>
+    //   <div className="flex gap-2 p-2">
+    //     <Link className="[&.active]:font-bold" to="/">
+    //       Home
+    //     </Link>{' '}
+    //     <Link className="[&.active]:font-bold" to="/about">
+    //       About
+    //     </Link>
+    //   </div>
+    //   <hr />
+    //   <Outlet />
+    //   <TanStackRouterDevtools />
+    // </>
   )
 });
