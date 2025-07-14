49202397eb3a8c01f6bdfe903dcbae9497d5c0c3
joshunrau <contact@joshuaunrau.com>
2025-07-14 13:12:46 -0400

    chore: update

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,29 +1,3 @@
-// import React from 'react';
-
-// import { NotificationHub } from '@douglasneuroinformatics/libui/components';
-// import { QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-// import { BrowserRouter } from 'react-router-dom';
-
-// import { Routes } from '@/Routes';
-// import { queryClient } from '@/services/react-query';
-
-// import { SetupProvider } from './features/setup';
-
-// export const App = () => {
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       <NotificationHub />
-//       <SetupProvider>
-//         <BrowserRouter>
-//           <Routes />
-//         </BrowserRouter>
-//       </SetupProvider>
-//       <ReactQueryDevtools />
-//     </QueryClientProvider>
-//   );
-// };
-
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
 
