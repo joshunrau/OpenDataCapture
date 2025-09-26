7bae98af9eb1e2f69146834a1f2048695fc12e56
joshunrau <contact@joshuaunrau.com>
2025-09-26 12:55:46 -0400

    update

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { $LoginCredentials } from '@opendatacapture/schemas/auth';
 
-@Controller('auth')
-export class AuthController {}
+import { RouteAccess } from '@/core/decorators/route-access.decorator.js';
+
+import { AuthService } from './auth.service.js';
+
+@Controller({ path: 'auth' })
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @ApiOperation({ summary: 'Login' })
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  @RouteAccess('public')
+  async login(@Body() credentials: $LoginCredentials): Promise<{ accessToken: string }> {
+    return this.authService.login(credentials);
+  }
+}
