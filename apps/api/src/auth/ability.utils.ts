87e79fde6bd1b85f0403355519dae8b5d94263f0
joshunrau <contact@joshuaunrau.com>
2025-09-26 13:28:47 -0400

    update

--- apps/api/src/auth/ability.utils.ts
+++ apps/api/src/auth/ability.utils.ts
@@ -1,9 +1,18 @@
 import { detectSubjectType } from '@casl/ability';
+import { createPrismaAbility } from '@casl/prisma';
 import type { AppSubject } from '@prisma/client';
+
+import type { AppAbilities, AppAbility, Permission } from './auth.types';
 
 export function detectAppSubject(obj: { [key: string]: any }) {
   if (typeof obj.__modelName === 'string') {
     return obj.__modelName as AppSubject;
   }
   return detectSubjectType(obj) as AppSubject;
+}
+
+export function createAppAbility(permissions: Permission[]): AppAbility {
+  return createPrismaAbility<AppAbilities>(permissions, {
+    detectSubjectType: detectAppSubject
+  });
 }
