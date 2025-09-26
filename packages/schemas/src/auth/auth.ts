4200702aa4f65ac600949c46b9ede004a1f5ed57
joshunrau <contact@joshuaunrau.com>
2025-09-26 12:47:50 -0400

    update

--- packages/schemas/src/auth/auth.ts
+++ packages/schemas/src/auth/auth.ts
@@ -8,7 +8,7 @@ export type AuthPayload = {
   accessToken: string;
 };
 
-export type LoginCredentials = z.infer<typeof $LoginCredentials>;
+export type $LoginCredentials = z.infer<typeof $LoginCredentials>;
 export const $LoginCredentials = z.object({
   password: z.string(),
   username: z.string()
