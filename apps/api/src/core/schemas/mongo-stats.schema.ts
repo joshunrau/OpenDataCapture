b5107825e62e6d590d6c07eceb0ac6bc3d467b4d
joshunrau <contact@joshuaunrau.com>
2025-09-26 17:50:41 -0400

    chore: update

--- apps/api/src/core/schemas/mongo-stats.schema.ts
+++ apps/api/src/core/schemas/mongo-stats.schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod/v4';
 
-export type MongoStats = z.infer<typeof $MongoStats>;
+export type $MongoStats = z.infer<typeof $MongoStats>;
 export const $MongoStats = z.object({
   collections: z.number(),
   db: z.string(),
