71bb6c331bef1c5593e5c8509599cfc8ff67aebe
joshunrau <contact@joshuaunrau.com>
2025-09-26 14:14:20 -0400

    update

--- apps/api/src/typings/global.d.ts
+++ apps/api/src/typings/global.d.ts
@@ -1,5 +1,13 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+
+import type { TokenPayload } from '@opendatacapture/schemas/auth';
 import type { ReleaseInfo } from '@opendatacapture/schemas/setup';
 
 declare global {
   const __RELEASE__: ReleaseInfo;
+  namespace Express {
+    interface User extends TokenPayload {
+      ability: AppAbility;
+    }
+  }
 }
