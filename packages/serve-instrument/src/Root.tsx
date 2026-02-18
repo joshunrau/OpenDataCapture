8a03d37da7da6a6e20186c9837d557c57d400a24
joshunrau <contact@joshuaunrau.com>
2026-02-18 17:38:33 -0500

    update

--- packages/serve-instrument/src/Root.tsx
+++ packages/serve-instrument/src/Root.tsx
@@ -1,5 +1,5 @@
 import type React from 'react';
-import { useState } from 'react';
+import { StrictMode, useState } from 'react';
 
 export type RootProps = {
   [key: string]: never;
@@ -9,9 +9,11 @@ export const Root: React.FC<RootProps> = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center' }}>
-      <h1>Counter: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-    </div>
+    <StrictMode>
+      <div style={{ fontFamily: 'sans-serif', textAlign: 'center' }}>
+        <h1>Counter: {count}</h1>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+    </StrictMode>
   );
 };
