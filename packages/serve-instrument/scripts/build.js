ddefee886e93a2be3b414aa3509bca1a63700ee9
joshunrau <contact@joshuaunrau.com>
2026-02-18 14:42:53 -0500

    update

--- packages/serve-instrument/scripts/build.js
+++ packages/serve-instrument/scripts/build.js
@@ -3,6 +3,8 @@ import * as path from 'path';
 
 import * as esbuild from 'esbuild';
 
+import pkg from '../package.json' with { type: 'json' };
+
 const outdir = path.resolve(import.meta.dirname, '../dist');
 
 await fs.promises.rm(outdir, { force: true, recursive: true });
@@ -13,28 +15,42 @@ await esbuild.build({
   },
   bundle: true,
   entryPoints: [path.resolve(import.meta.dirname, '../src/cli.ts')],
+  external: [...Object.keys(pkg.dependencies), 'esbuild-wasm'],
   format: 'esm',
-  loader: {
-    '.node': 'copy'
-  },
   minify: false,
   outdir,
-  platform: 'node',
-  plugins: [
-    {
-      name: 'external',
-      setup(build) {
-        build.onResolve({ filter: /.*/, namespace: 'file' }, async (args) => {
-          const resolved = await build.resolve(args.path, {
-            kind: args.kind,
-            resolveDir: args.resolveDir
-          });
-          if (resolved.path.includes('node_modules')) {
-            return { external: true, path: args.path };
-          }
-          return null;
-        });
-      }
-    }
-  ]
+  platform: 'node'
+  // plugins: [
+  //   {
+  //     name: 'external',
+  //     setup(build) {
+  //       const dependencies = Object.keys(pkg.dependencies);
+  //       build.onResolve({ filter: /.*/, namespace: 'file' }, async (args) => {
+  //         const resolved = await build.resolve(args.path, {
+  //           kind: args.kind,
+  //           resolveDir: args.resolveDir
+  //         });
+
+  //         if (resolved.path.includes('node_modules')) {
+  //           const packageName = args.path.match(/^(@[^/]+\/[^/]+|[^/]+)/)?.at(1);
+  //           if (!packageName) {
+  //             throw new Error(`Cannot determine package for import path: ${args.path}`);
+  //           } else if (dependencies.includes(packageName)) {
+  //             return { external: true, path: args.path };
+  //           }
+
+  //           console.log(packageName);
+
+  //           return null
+  //         }
+
+  //         return null;
+  //       });
+  //     }
+  //   }
+  // ]
+});
+
+await fs.promises.cp(path.resolve(import.meta.dirname, '../src/client'), path.resolve(outdir, 'client'), {
+  recursive: true
 });
