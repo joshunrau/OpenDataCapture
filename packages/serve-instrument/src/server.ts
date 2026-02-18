2dbada801455d522ae8293c046111be2a3d7a589
joshunrau <contact@joshuaunrau.com>
2026-02-18 17:07:48 -0500

    update

--- packages/serve-instrument/src/server.ts
+++ packages/serve-instrument/src/server.ts
@@ -2,12 +2,26 @@
 
 import * as http from 'node:http';
 
-export function createServer(port: number) {
-  const server = http.createServer((req, res) => {
+import { generateMetadata } from '@opendatacapture/runtime-meta';
+
+export async function createServer(port: number) {
+  const metadata = await generateMetadata();
+
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  const server = http.createServer(async (req, res) => {
     if (req.method === 'GET') {
       if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end('<h1>Hello World</h1>');
+      } else if (req.url?.startsWith('/runtime')) {
+        const [version, ...paths] = req.url.split('/').filter(Boolean) ?? [];
+        const filepath = paths.join('/');
+        if (!(version && filepath) || !metadata.has(version)) {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end('Not Found');
+        }
+
+        const { baseDir, manifest } = metadata.get(version!)!;
       } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Not Found');
@@ -17,6 +31,7 @@ export function createServer(port: number) {
       res.end('Method Not Allowed');
     }
   });
+
   return new Promise<void>((resolve) => {
     server.listen(port, () => {
       console.log(`Listening on http://localhost:${port}`);
