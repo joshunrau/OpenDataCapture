77fe11c746b0121359b7e74f1228c4b683d896cf
joshunrau <contact@joshuaunrau.com>
2026-02-18 13:14:55 -0500

    update

--- packages/serve-instrument/scripts/build.js
+++ packages/serve-instrument/scripts/build.js
@@ -5,14 +5,18 @@ import * as esbuild from 'esbuild';
 
 const outdir = path.resolve(import.meta.dirname, '../dist');
 
+import pkg from '../package.json' with { type: 'json' };
+
 await fs.promises.rm(outdir, { force: true, recursive: true });
 
 await esbuild.build({
   banner: {
     js: '#!/usr/bin/env node'
   },
-  // bundle: true,
+  bundle: true,
   entryPoints: [path.resolve(import.meta.dirname, '../src/cli.ts')],
+  external: Object.keys(pkg.dependencies),
+  format: 'esm',
   minify: false,
   outdir,
   platform: 'node'
