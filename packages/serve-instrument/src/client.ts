54700c850d86dc856a928dacd97405cdab680db8
joshunrau <contact@joshuaunrau.com>
2026-02-18 17:25:00 -0500

    update

---
+++ packages/serve-instrument/src/client.ts
@@ -0,0 +1,5 @@
+import { StrictMode, useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
---
+++ packages/serve-instrument/src/entry-server.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import { Root } from './Root';
+
+import type { RootProps } from './Root';
+
+export type RenderFunction = (props: RootProps) => { html: string };
+
+export const render: RenderFunction = (props) => {
+  const html = ReactDOMServer.renderToString(
+    <React.StrictMode>
+      <Root {...props} />
+    </React.StrictMode>
+  );
+  return { html };
+};
